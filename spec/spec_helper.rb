d3a5c1979946ba73c0de9dd4a71195081219e55f
Henri Watson <henri@henriwatson.com>
2015-03-21 16:02:16 -0400

    add codeclimate hooks to travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 require "simplecov"
 
-# Only use Coveralls when being run in CI
+# Only use CodeClimate when being run in CI
 if ENV["CI"]
-  require "coveralls"
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  require "codeclimate-test-reporter"
+  SimpleCov.formatter = CodeClimate::TestReporter::Formatter
 end
 
 SimpleCov.start do
@@ -15,4 +15,4 @@ require "powerapi"
 require "savon/mock/spec_helper"
 
 require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(:allow => [/localhost/, /codeclimate.com/])
